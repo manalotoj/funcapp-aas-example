66dec99fc90308167030fb680dc1279a37f5d2cc
John Manaloto <manaloto@macbook-pro.lan>
2020-08-26 14:26:47 -0700

    use managed identity

--- src/netfx/Aas/Aas.FuncApp/Functions/WebAppHttpTrigger.cs
+++ src/netfx/Aas/Aas.FuncApp/Functions/WebAppHttpTrigger.cs
@@ -9,17 +9,21 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Aas.FuncApp.Services;
 using System.Net.Http;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
 
 namespace Aas.FuncApp.Functions
 {
   public class WebAppHttpTrigger
   {
+    private readonly IConfiguration config;
     private readonly AzureAdService adService;
     private readonly AzureWebSiteService webAppService;
     private readonly ILogger<WebAppHttpTrigger> log;
 
-    public WebAppHttpTrigger(AzureAdService adService, AzureWebSiteService webAppService, ILogger<WebAppHttpTrigger> log)
+    public WebAppHttpTrigger(IConfiguration config, AzureAdService adService, AzureWebSiteService webAppService, ILogger<WebAppHttpTrigger> log)
     {
+      this.config = config;
       this.adService = adService;
       this.webAppService = webAppService;
       this.log = log;
@@ -33,6 +37,12 @@ namespace Aas.FuncApp.Functions
       log.LogTrace($"subscriptionId: {subscriptionId}, resource group: {group}, app: {app}");
       var accessToken = await adService.GetAccessTokenAsync();
       log.LogTrace($"accessToken: {accessToken}");
+
+      var managedIdentityId = config.GetValue<string>("ManagedIdentityId");
+      var connectionString = "RunAs=App;AppId=" + managedIdentityId + $";TenantId={config.GetValue<string>("tenantId")}";
+      var tokenProvider = new AzureServiceTokenProvider(connectionString);
+      accessToken = await tokenProvider.GetAccessTokenAsync(config.GetValue<string>("ClientScope"));
+
 
       var credentials = adService.GetCredentials();
       var ipAddressList = await webAppService.GetPossibleOutboundIpAddressesAsync(credentials, subscriptionId, group, app);
